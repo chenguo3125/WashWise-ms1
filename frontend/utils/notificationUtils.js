cfad895752592492fa6c051a838bc92870661b8b
Guo Chen <guochen@Guos-MacBook-Air-3.local>
2025-06-22 18:53:34 +0800

    Add the notification feature

---
+++ frontend/utils/notificationUtils.js
@@ -0,0 +1,36 @@
+import * as Device from 'expo-device';
+import * as Notifications from 'expo-notifications';
+import { Platform } from 'react-native';
+
+export async function registerForPushNotificationsAsync() {
+  let token;
+
+  if (Device.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+
+    if (finalStatus !== 'granted') {
+      alert('Permission for notifications not granted!');
+      return null;
+    }
+
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    console.log('Push token:', token);
+  } else {
+    alert('Push notifications require a physical device.');
+  }
+
+  if (Platform.OS === 'android') {
+    await Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.DEFAULT,
+    });
+  }
+
+  return token;
+}
